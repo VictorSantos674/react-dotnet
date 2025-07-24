3194916faa77c03fa1d416f1af94787a9cfcb770
Santos_Vic <142180844+VictorSantos674@users.noreply.github.com>
2025-07-24 14:57:13 +0000

    feat(repository): adiciona contrato de métodos CRUD em IProdutoRepository

--- backend/Domain/Interfaces/IProdutoRepository.cs
+++ backend/Domain/Interfaces/IProdutoRepository.cs
@@ -4,7 +4,11 @@ namespace Domain.Interfaces
 {
     public interface IProdutoRepository
     {
-        Task Add(Produto produto);
-        Task<List<Produto>> GetAll();
+        Task Add(Produto entity);
+        Task Update(Produto entity);
+        Task Delete(Produto entity);
+        Task<Produto> GetEntityById(int id);
+        Task<List<Produto>> GetList();
+        Task<(List<Produto> itens, int total)> GetPagedList(int pageNumber, int pageSize, string searchTerm, string searchProperty, string orderByProperty, bool isAscending);
     }
 }
