f70135c92185b25e183a7b5b88e56b18d0561c2d
Santos_Vic <142180844+VictorSantos674@users.noreply.github.com>
2025-07-24 14:56:47 +0000

    feat(service): adiciona contrato de métodos CRUD em IProdutoService

--- backend/Domain/Interfaces/IProdutoService.cs
+++ backend/Domain/Interfaces/IProdutoService.cs
@@ -5,6 +5,18 @@ namespace Domain.Interfaces
     public interface IProdutoService
     {
         Task AddProdutoAsync(Produto produto);
-        Task<List<Produto>> GetProdutosAsync();
+        Task UpdateProdutoAsync(Produto produto);
+        Task DeleteProdutoAsync(Produto produto);
+        Task<Produto> GetProdutoByIdAsync(int id);
+        Task<List<Produto>> GetAllProdutosAsync();
+        Task<(List<Produto> itens, int total)> GetPagedList(
+            int pageNumber,
+            int pageSize,
+            string searchTerm,
+            string searchProperty,
+            string orderByProperty,
+            bool isAscending
+        );
+        
     }
 }
