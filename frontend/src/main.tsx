55b760220ee5c3f1928c19be7181f9d165856a97
Santos_Vic <142180844+VictorSantos674@users.noreply.github.com>
2025-07-24 13:54:50 +0000

    chore(frontend): conecta a store Redux ao React com Provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./store";
 import router from "./routes";
 import Navbar from "./components/Navbar";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Navbar />
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <Navbar />
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
