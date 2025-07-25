8055b068981f5a9fc13475a4b030c856f3b7c99f
Santos_Vic <142180844+VictorSantos674@users.noreply.github.com>
2025-07-25 12:41:09 +0000

    chore: configure Redux store with RTK Query middleware

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,16 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { setupListeners } from "@reduxjs/toolkit/query";
-import { api } from "./api";
+import { productApi } from '@/services/api/endpoints/productApi';
 
 export const store = configureStore({
   reducer: {
-    [api.reducerPath]: api.reducer,
+    [productApi.reducerPath]: productApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(api.middleware),
+    getDefaultMiddleware().concat(productApi.middleware),
 });
-
-setupListeners(store.dispatch);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
