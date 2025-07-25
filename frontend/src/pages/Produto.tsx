e9ba83b73ec02bd257157f5934dfa48b96cb83cf
Santos_Vic <142180844+VictorSantos674@users.noreply.github.com>
2025-07-25 12:38:54 +0000

    feat: implement addProduct mutation using RTK Query

--- frontend/src/pages/Produto.tsx
+++ frontend/src/pages/Produto.tsx
@@ -1,13 +1,27 @@
-import { useParams } from "react-router-dom";
+import { useAddProductMutation } from '@/services/api/endpoints/productApi';
+import { useState } from 'react';
 
-function Produto() {
-  const { id } = useParams();
+export default function Produto() {
+  const [nome, setNome] = useState('');
+  const [preco, setPreco] = useState(0);
+  const [descricao, setDescricao] = useState('');
+
+  const [addProduct] = useAddProductMutation();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    await addProduct({ nome, preco, descricao });
+    setNome('');
+    setPreco(0);
+    setDescricao('');
+  };
 
   return (
-    <div>
-      <h1>Detalhes do Produto</h1>
-      <p>ID do Produto: {id}</p>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <input value={nome} onChange={(e) => setNome(e.target.value)} placeholder="Nome" />
+      <input type="number" value={preco} onChange={(e) => setPreco(+e.target.value)} placeholder="Preço" />
+      <textarea value={descricao} onChange={(e) => setDescricao(e.target.value)} placeholder="Descrição" />
+      <button type="submit">Cadastrar Produto</button>
+    </form>
   );
 }
-export default Produto;
