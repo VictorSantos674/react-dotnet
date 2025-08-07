ece139a547e3f3fdb9971a4a6fa1159ecfddc28f
Santos_Vic <142180844+VictorSantos674@users.noreply.github.com>
2025-08-07 18:58:35 +0000

    refactor(core): ajustes em App e main para refletir novas configurações globais

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './store';
 import { ConfigProvider, theme as antdTheme } from 'antd';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ConfigProvider
-      theme={{
-        algorithm: antdTheme.defaultAlgorithm,
-        token: {
-          colorPrimary: '#722ED1',
-          borderRadius: 8,
-          fontFamily: 'Inter, sans-serif',
-        },
-      }}
-    >
-      <App />
-    </ConfigProvider>
-  </React.StrictMode>,
+    <Provider store={store}>
+      <ConfigProvider
+        theme={{
+          algorithm: antdTheme.defaultAlgorithm,
+          token: {
+            colorPrimary: '#722ED1',
+            borderRadius: 8,
+            fontFamily: 'Inter, sans-serif',
+          },
+        }}
+      >
+        <App />
+      </ConfigProvider>
+    </Provider>
+  </React.StrictMode>
 );
