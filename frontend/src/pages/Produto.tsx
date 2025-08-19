bbd27944c67a7bc6782e1d386c54e3d19fd09907
Santos_Vic <142180844+VictorSantos674@users.noreply.github.com>
2025-08-19 00:45:27 +0000

    refact(Produto): aplicar novo padrão visual nos produtos com Card e Title

--- frontend/src/pages/Produto.tsx
+++ frontend/src/pages/Produto.tsx
@@ -1,8 +1,11 @@
+import { Typography, message } from 'antd';
+import Card from 'antd/es/card/Card';
 import ProductForm from '@/components/ProductForm';
 import { useAddProductMutation } from '@/services/api/endpoints/productApi';
 import type { ProductFormValues } from '@/validations/productFormSchema';
-import { message, Typography } from 'antd';
 import { useNavigate } from 'react-router-dom';
+
+const { Title } = Typography;
 
 export default function Produto() {
   const [addProduct, { isLoading }] = useAddProductMutation();
@@ -19,9 +22,31 @@ export default function Produto() {
   };
 
   return (
-    <div style={{  maxWidth: 600, margin: '0 auto', padding: '1rem'  }}>
-      <Typography.Title level={2}>Cadastrar Produto</Typography.Title>
-      <ProductForm onSubmit={handleSubmit} loading={isLoading} submitText="Criar Produto"/>
+    <div style={{ maxWidth: 700, margin: '0 auto', padding: '2rem' }}>
+      <Card
+        style={{
+          background: 'var(--color-card)',
+          borderRadius: '12px',
+          boxShadow: '0 4px 10px rgba(0,0,0,0.08)',
+        }}
+      >
+        <Title
+          level={3}
+          style={{
+            textAlign: 'center',
+            marginBottom: '1.5rem',
+            color: 'var(--color-primary)',
+          }}
+        >
+          Cadastrar Produto
+        </Title>
+
+        <ProductForm
+          onSubmit={handleSubmit}
+          loading={isLoading}
+          submitText="Criar Produto"
+        />
+      </Card>
     </div>
   );
 }
