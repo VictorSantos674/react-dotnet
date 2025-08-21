0aa7e091befedef67f558ea91976956951ce05ed
Santos_Vic <142180844+VictorSantos674@users.noreply.github.com>
2025-08-21 17:20:38 +0000

    fix: corrigir importações.

--- frontend/src/services/api/endpoints/productApi.ts
+++ frontend/src/services/api/endpoints/productApi.ts
@@ -1,5 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import type { Product, PaginatedProductsResponse, ProductFilters } from '@/types/Product';
+import type { Product, PaginatedProductsResponse } from '@/types/Product';
 
 export const productApi = createApi({
   reducerPath: 'productApi',
